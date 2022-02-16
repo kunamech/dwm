53dc9d07b52f42fd8d1246d7a2cc3f0496938e33
Thesourtimes <cckhmck@gmail.com>
2022-02-16 05:04:18 +0300

    :skull:

--- config.h
+++ config.h
@@ -50,11 +50,6 @@ static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
-static const LayoutMonitorRule lm_rules[] = {
-		/* >=w, >=h, req'd layout, new nmaster, new mfact */
-			{ 3000, 0,   0,            2,           0.66 },
-};
-
 #include "fibonacci.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
